94617078a44e86f20a7e1ba8a2d9e48dc3e5e4da
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-10-13 22:30:08 +0300

    make BString accept String

--- src/test/java/com/mykola2312/retracker/bencode/EqualsTest.java
+++ src/test/java/com/mykola2312/retracker/bencode/EqualsTest.java
@@ -11,4 +11,10 @@ public class EqualsTest {
 		assertEquals(new BInteger(1), new BInteger(1));
 		assertNotEquals(new BInteger(2), new BInteger(3));
 	}
+	
+	@Test
+	public void testStringEquals() {
+		assertEquals(new BString("first"), new BString("first"));
+		assertNotEquals(new BString("first"), new BString("second"));
+	}
 }
