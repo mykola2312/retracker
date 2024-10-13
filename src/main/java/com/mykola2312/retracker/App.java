9633383a990a3e950dafaba83e523fddd38be61f
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-10-13 04:34:19 +0300

    use tabs instead of spaces

--- src/main/java/com/mykola2312/retracker/App.java
+++ src/main/java/com/mykola2312/retracker/App.java
@@ -4,7 +4,7 @@ package com.mykola2312.retracker;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+    	System.out.println("Hello World!");
+	}
 }
