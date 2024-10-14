e6b054c2b8a768f7931136fa12b351ae20c6abc5
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-10-14 18:33:24 +0300

    implement get chaining, cover by tests

--- src/main/java/com/mykola2312/retracker/bencode/error/BErrorValueCast.java
+++ src/main/java/com/mykola2312/retracker/bencode/error/BErrorValueCast.java
@@ -1,11 +1,12 @@
 package com.mykola2312.retracker.bencode.error;
 
+import com.mykola2312.retracker.bencode.BType;
 import com.mykola2312.retracker.bencode.BValue;
 
 public class BErrorValueCast extends BValueError {
 	private static final long serialVersionUID = 6790204193944478194L;
 
-	public BErrorValueCast(BValue node, String key, Class cause) {
-		super(node, String.format("cast error: %s is instance of %s", key, cause.getName()));
+	public BErrorValueCast(BValue node, String key, BType wanted, BType found) {
+		super(node, String.format("cast error: %s is type %s instead of %s", key, found, wanted));
 	}
 }
